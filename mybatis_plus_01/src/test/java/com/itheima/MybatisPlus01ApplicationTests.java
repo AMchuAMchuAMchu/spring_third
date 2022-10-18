cdf1f47023500761a1aaf4c1baa53cddf113a120
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-18 17:48:07 +0800

    AM 01st commit

--- mybatis_plus_01/src/test/java/com/itheima/MybatisPlus01ApplicationTests.java
+++ mybatis_plus_01/src/test/java/com/itheima/MybatisPlus01ApplicationTests.java
@@ -30,7 +30,7 @@ class MybatisPlus01ApplicationTests {
 		QueryWrapper<AnimeInfo> qw = new QueryWrapper<>();
 
 //		qw.select("count(*) count");
-		qw.select("max(releaseTime) max");
+		qw.select("max(releaseTime) as max");
 //		qw.select("min(release_time) min");
 
 		List<Map<String, Object>> maps = animeInfoDao.selectMaps(qw);
