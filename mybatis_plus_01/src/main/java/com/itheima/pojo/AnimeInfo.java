59f61a6e8b93e0f103c0e9465d013e9a173c7494
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-18 15:48:27 +0800

    AM 01st commit

---
+++ mybatis_plus_01/src/main/java/com/itheima/pojo/AnimeInfo.java
@@ -0,0 +1,26 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_third
+ * BelongsPackage ==> com.itheima.pojo
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-18 15:44:58
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class AnimeInfo {
+
+    private String name;
+
+    private Integer releaseTime;
+
+    private String character01;
+
+    private String character02;
+
+    private Integer id;
+
+}
---
+++ src/test/java/com/itheima/spring_third/SpringThirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.spring_third;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
