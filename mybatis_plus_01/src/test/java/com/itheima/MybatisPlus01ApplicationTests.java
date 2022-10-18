383d9f73e0001d21567c46c333cd0520afb8b489
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-18 16:24:27 +0800

    AM 01st commit

--- mybatis_plus_01/src/test/java/com/itheima/MybatisPlus01ApplicationTests.java
+++ mybatis_plus_01/src/test/java/com/itheima/MybatisPlus01ApplicationTests.java
@@ -32,7 +32,7 @@ class MybatisPlus01ApplicationTests {
 //		lqw.le(AnimeInfo::getReleaseTime,2020);
 //		lqw.ge(AnimeInfo::getReleaseTime,2020);
 //		lqw.ge(AnimeInfo::getReleaseTime,2010).le(AnimeInfo::getReleaseTime,2018);
-		lqw.le(AnimeInfo::getReleaseTime,2012).or().ge(AnimeInfo::getReleaseTime,2018);
+		lqw.le(AnimeInfo::getReleaseTime,2012).or().gt(AnimeInfo::getReleaseTime,2018);
 
 		List<AnimeInfo> animeInfos = animeInfoDao.selectList(lqw);
 		animeInfos.forEach(System.out::println);
