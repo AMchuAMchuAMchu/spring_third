86408615e16916dc2cd0d141b339a12f01013efe
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-18 15:54:58 +0800

    AM 01st commit

---
+++ mybatis_plus_01/src/main/java/com/itheima/config/MpConfigTest.java
@@ -0,0 +1,29 @@
+package com.itheima.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_third
+ * BelongsPackage ==> com.itheima.config
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-18 15:51:24
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Configuration
+public class MpConfigTest {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mp = new MybatisPlusInterceptor();
+        mp.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mp;
+    }
+
+
+
+
+}
--- mybatis_plus_01/src/test/java/com/itheima/MybatisPlus01ApplicationTests.java
+++ mybatis_plus_01/src/test/java/com/itheima/MybatisPlus01ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.itheima;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.dao.AnimeInfoDao;
 import com.itheima.pojo.AnimeInfo;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,18 @@ class MybatisPlus01ApplicationTests {
 
 	@Autowired
 	private AnimeInfoDao animeInfoDao;
+
+	@Test
+	void testPage(){
+
+		Page<AnimeInfo> animeInfoPage = new Page<>(1, 4);
+
+		Page<AnimeInfo> animeInfoPage1 = animeInfoDao.selectPage(animeInfoPage, null);
+
+		animeInfoPage1.getRecords().forEach(System.out::println);
+
+	}
+
 
 	@Test
 	void contextLoads() {
